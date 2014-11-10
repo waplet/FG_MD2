c9f36df1384ca349b9d209b1016c462bafeab180
waplet <maarisjankovskis@gmail.com>
2014-11-10 23:39:34 +0200

    renamed

--- md2.php
+++ md2.php
@@ -1,8 +1,10 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 'on');
 /** 
  * Otrais neobligātais mājasdarbs
+ * Māris Jankovskis, mj12015
+ * aaaabbaabbbacbbacac - pieder
+ * bbbaaacaaaacbbbbbbc - pieder
+ * aaaabbaaaaaaaacaaac - nepieder
  */
 function pr($arg) {
 	echo "<pre>". print_r($arg, true);
@@ -15,9 +17,9 @@ class FindWord {
 	private $productions = [
 		'S' => [
 			'aaAbb', 
-			'Sbb', 
 			'aabb', 
 			'bb' ,
+			'Sbb', 
 			'FFaFc', 
 			'FaFc' ,
 			'aFc',
@@ -34,62 +36,68 @@ class FindWord {
 	];
 
 	private $nonterms = 'S|A|F';
-	private $terms = 'a|b|c';
-	private $found = false;
-	private $symbol_pos;
-
+	private $found    = false;
+	/**
+	 * Inicializē un pieglabā meklējamo vārdu un tā garumu
+	 * @param string $w meklējamais vārds
+	 */
 	function __construct($w) {
 		$this->search_word = $w;
 		$this->search_length = strlen($w);
 	}
 
-	public function checkword($word, $level = 1, $maxlevel) {
-		//if($level == $maxlevel) return;
+	/**
+	 * Meklē jaunus vārdus un salīdzina ar meklējamo
+	 * @param  string $word jaunais vārds
+	 * @return void       does nothing
+	 */
+	public function checkword($word) {
+		/**
+		 * Ja vārds atrasts, ejam ārā
+		 */
 		if($word == $this->search_word) {
-			$this->found = true;
+			$this->found = true; return;
+		}
+		/**
+		 * Ja vārda garums pārsniedz, metam nost vārdu
+		 */
+		if(strlen($word) > $this->search_length) {
 			return;
 		}
-		if($this->found) return;
-		if(strlen($word) >= $this->search_length && $word != $this->search_word) {
-			//echo $word.'- garaaks<br/>';
-			return;
-		} 
-		$matches = preg_split('[' . $this->nonterms .']', $word);
-		// pr($matches);	
-		//echo "<hr/>";
-		// if(count($matches) == 1 && $matches[0] == $this->search_word) {
-		// 	$this->found = true;
-		// 	return;
-		// }
-		if($this->needlesInWord($matches)) { // vārds ir derīgs
+
+		/**
+		 * Atrodam visas termināļu virknītes
+		 */
+		$matches = (preg_split('/[' . $this->nonterms .']+/', $word));
+
+		/**
+		 * Pārbaudām vai visas virknes ir vārdā
+		 */
+		if($this->needlesInWord($matches)) {
+			/** 
+			 * Kreisējā netermināļa atrašana
+			 */
 			$symbol_pos = (count($matches) > 1) ? strlen($matches[0]) : -1;
 			if($symbol_pos < 0) return;
 			$symbol = isset($word[$symbol_pos]) ? $word[$symbol_pos] : null;
-			
-			if(!$symbol && $this->search_word == $matches[0]) {
-				$this->found = true;
-				return;
-			}
-
 			if(!$symbol) return;
-			// die(pr($this->symbol));
+			/**
+			 * Produkciju pielietošana vārdam
+			 */
 			foreach($this->productions[$symbol] as $p) {
-				// echo $word.'- vaards';
-				// // echo $this->symbol_pos;
-				// echo $p;echo "<br/>";
 				if($this->found) return;
-
-				$newword = mb_substr($word,0,$symbol_pos).$p.mb_substr($word,$symbol_pos+1);
-				echo $level.'-'.$word.'-'.$newword.'-'.$symbol_pos."<br/>";
-				//echo $word;
-				$this->checkword($newword,$level +1, $level);
-
+				$this->checkword(mb_substr($word,0,$symbol_pos).$p.mb_substr($word,$symbol_pos+1));
 			}
 		}
-		return false;
+		return;
 	}
 
-	public function needlesInWord($needles) {
+	/**
+	 * Skatās vai visas ntermināļu virknītes ir meklējamajā vārdā
+	 * @param  array $needles array of nonterminals strings
+	 * @return boolean          true/false
+	 */
+	private function needlesInWord($needles) {
 		foreach($needles as $n) {
 			if (!empty($n) && strpos($this->search_word, $n) === false) {
 				return false;
@@ -98,16 +106,24 @@ class FindWord {
 		return true;
 	}
 
+	/**
+	 * Galvenā rezultāta atgriešana
+	 * @return boolean true/false
+	 */
 	public function found() {
 		return $this->found;
 	}
+
+	/**
+	 * atgriež meklējamo vārdu
+	 * @return [type] [description]
+	 */
+	public function getWord() {
+		return $this->search_word;
+	}
 }
-// aaabbaabbbacbbacac
-// bbbaaacaaaacbbbbbbc
-// aaaabbaaaaaaaacaaac
-// $s = 'aabb';
-// $sx = substr('Sbb',0,0 ).$s.substr('Sbb',1);
-// var_dump($sx); die;
+$start = microtime(true);
 $x = new FindWord('aaaabbaaaaaaaacaaac');
-$x->checkword('S',1,10);
-var_dump($x->found());
+$x->checkword('S');
+echo $x->getWord() . ' '. ($x->found() ? "Pieder" : "Nepieder");
+echo "<br/>" . (microtime(true)-$start);
